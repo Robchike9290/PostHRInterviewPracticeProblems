368784a03fa9d1338278c2f48c27d45077ebca81
Robert Czajka <robchike9290@gmail.com>
2022-05-25 22:01:15 -0500

    Remove tests and comments from selectionSort file, added selectionSort tests to testing file.

--- selectionSort.js
+++ selectionSort.js
@@ -1,28 +1,10 @@
-let test = [5, 66, 7, -4, -109, 88];
-let test2 = [10, 0, 0, 4, -3, 8];
-let test3 = [8, NaN, 'hammer', true];
-let test4 = true;
-
 const selectionSort = function (array) {
-  // I - An array
-  // O - A sorted array
-  // C - None
-  // E - The input may have only one or zero elements, and thus could be considered sorted.
-  //   - The input array may contain elements that are not numbers, in which case an error should be thrown
-  //   - The input not be an array, in which case an error should be thrown.
 
-  // Address the edge cases first, strating with error handling.
   if (!Array.isArray(array)) throw "The input must be an array.";
   for (let i = 0; i < array.length; i++) {
     if (typeof array[i] !== 'number') throw "Some of the indexes in the array input were ont numerical"
   }
   if (array.length < 2) return array;
-
-  // Loop through the array from beginning to end
-    // Set a placeholder variable to keep track of the minimum element, set to the current element
-    // Have an inner loop over the array from beginning to end
-      // If the current element is less than the minimum element, reset the variable to this element
-    // Swap the current element and the minimum element if they are not equal.
 
   for (let i = 0; i < array.length; i++) {
     let smallestValue = array[i];
@@ -39,11 +21,8 @@ const selectionSort = function (array) {
       array[smallestValueIndex] = temp;
     }
   }
-  // Return the sorted array.
+
   return array;
 }
 
-// selectionSort(test);
-// selectionSort(test2);
-// selectionSort(test3);
-// selectionSort(test4);
+module.exports = selectionSort;
--- tp.spec.js
+++ tp.spec.js
@@ -1,5 +1,6 @@
 const bubbleSort = require("./bubbleSort");
 const insertionSort = require("./insertionSort");
+const selectionSort = require("./selectionSort");
 
 describe("bubbleSort", function(){
   it("Should sort an array of random whole numbers correctly.", function(){
@@ -38,5 +39,25 @@ describe("insertionSort", function(){
 
   it("Should throw an error if a non-array input is tried.", function(){
     expect(() => insertionSort('Array, what\'s that???')).toThrow('The input was not an array.');
+  })
+})
+
+describe("selectionSort", function(){
+  it("Should sort an array of random whole numbers correctly.", function(){
+    expect(selectionSort([5, 66, 7, -4, -109, 88])).toStrictEqual([-109, -4, 5, 7, 66, 88]);
+    expect(selectionSort([10, 0, 0, 4, -3, 8])).toStrictEqual([-3, 0, 0, 4, 8, 10]);
+  });
+
+  it("Should sort an array of random non-whole numbers correctly.", function(){
+    expect(selectionSort([-2.4, -578, 8.2, 408, 7, 7.7])).toStrictEqual([-578, -2.4, 7, 7.7, 8.2, 408]);
+    expect(selectionSort([-8, 7.48, 38, 9, -50.38])).toStrictEqual([-50.38, -8, 7.48, 9, 38]);
+  })
+
+  it("Should throw an error if a non-numerical index is in the input array.", function(){
+    expect(() => selectionSort([8, NaN, 'hammer', true])).toThrow('Some of the indexes in the array input were ont numerical');
+  })
+
+  it("Should throw an error if a non-array input is tried.", function(){
+    expect(() => selectionSort(true)).toThrow('The input must be an array.');
   })
 })
