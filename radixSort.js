e5a8db85dcfc7c2a78b5355d82f66854ca2414d0
Robert Czajka <robchike9290@gmail.com>
2022-05-28 12:37:45 -0500

    Write code for edge cases for radixSort and pass tests for them.

--- radixSort.js
+++ radixSort.js
@@ -1,7 +1,14 @@
 const radixSort = function(input) {
   // Reject non-array inputs
+  if (!Array.isArray(input)) throw "The input to this function must be an array";
   // Reject arrays with non-numerical and non-integer inputs
+  for (let element of input) {
+    if (typeof element !== "number" || element % 1 !== 0) {
+      throw "All the indexes in the input array must be whole integers"
+    }
+  }
   // Return arrays with zero or single indexes without running them through the sort algorithm
+  if (input.length < 2) return input;
 
   // Sort all the other possibilites
 
