b3ea19bded324fd65fa422b4634af97fb1954602
Robert Czajka <robchike9290@gmail.com>
2022-05-28 12:27:50 -0500

    Write basic pseudocode for radixSort

--- radixSort.js
+++ radixSort.js
@@ -1,5 +1,14 @@
 const radixSort = function(input) {
+  // Reject non-array inputs
+  // Reject arrays with non-numerical and non-integer inputs
+  // Return arrays with zero or single indexes without running them through the sort algorithm
 
+  // Sort all the other possibilites
+
+  // Sorting case for negative integers
+  // Sorting case of postive integers and zero
+
+  // Return the sorted input
 }
 
 module.exports = radixSort;
