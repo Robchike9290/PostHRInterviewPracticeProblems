5a5966aa48471bd30be190b9430f12d22aca12ba
Robert Czajka <robchike9290@gmail.com>
2022-05-29 23:16:02 -0500

    Create pseudocode for what the base functionality of the singly linked list should be.

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -1,3 +1,9 @@
+// Create a class to instantiate new singly linked lists with.  Make sure that it has...
+// - The ability to push new nodes onto the list with 'next' and 'value' properties
+// - The ability to keep track of the length of the list
+// - The ability to keep track of which node is the head of the list.
+// - The ability to keep track of which node is the tail of the list.
+
 class SinglyLinkedList() {
 
 }
